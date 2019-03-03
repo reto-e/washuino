640e9bee617cd77d320d5fca57f082dedbd1e982
Reto E <teig@bahngeil.ch>
2019-03-03 12:17:31 +0100

    changed double to float for average light value, updated trigger value.

--- washuino.ino
+++ washuino.ino
@@ -10,53 +10,11 @@
 #define RETO_CHANNEL 1
 
 
-int mic_val; // value from mic
-int distance[5]; // array for distances between sounds
-int trigger_volume; // if a sound is louder then this it starts the pattern watching loop
-int threshold = 20;
+int ldr_val; // value from light dependent resistor (ldr)
+int val_sum; //
+int trigger_value; // if a sound is louder then this it starts the pattern watching loop
 bool goIntoLoop = false;
 bool messageSent = false;
-
-unsigned long initial_millis;
-unsigned long a_millis;
-unsigned long b_millis;
-
-bool checkForDingDong(int distance[]) {
-  bool isDingDong = false;
-  if (distance[0] > 400 - threshold && distance[0] < 400 + threshold &&
-      distance[1] > 400 - threshold && distance[1] < 400 + threshold &&
-      distance[2] > 1200 - threshold && distance[2] < 1200 + threshold &&
-      distance[3] > 400 - threshold && distance[3] < 400 + threshold &&
-      distance[4] > 400 - threshold && distance[4] < 400 + threshold) isDingDong = true;
-  return isDingDong;
-}
-
-void listenToLoudSounds() {
-  mic_val = analogRead(A0);
-  if (mic_val > trigger_volume) {
-    initial_millis = millis();
-    a_millis = millis();
-    int i = 0;
-    while (millis() - initial_millis < 3000) {
-      mic_val = analogRead(A0);
-
-      // If there is another loud sound at least 200 milliseconds later
-      if (mic_val > trigger_volume && millis() - a_millis > 200) {
-        distance[i] = millis() - a_millis;
-        a_millis = millis();
-        if (i < 4) {
-          i++;
-        }
-      }
-    }
-    Serial.println(distance[0]);
-    Serial.println(distance[1]);
-    Serial.println(distance[2]);
-    Serial.println(distance[3]);
-    Serial.println(distance[4]);
-  }
-}
-
 
 
 //-------------------------------------------------
@@ -66,9 +24,9 @@ void listenToLoudSounds() {
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Cayenne.begin(username, password, clientID, ssid, wifiPassword);
+  Cayenne.begin(username, password, clientID, ssid, wifiPassword);  // comment out for debugging
 
-  trigger_volume = 500;
+  trigger_value = 30;
 
   
 }
@@ -77,22 +35,28 @@ void setup() {
 //   Loop
 //-------------------------------------------------
 void loop() {
+
+  for (int i = 0; i < 10; i++) {
+    ldr_val = analogRead(A0);
+    val_sum = val_sum + ldr_val;
+  }
+
+  float flat_val = val_sum/10;
   
-  listenToLoudSounds();
   
-  if (checkForDingDong(distance)) {
+  Serial.println(flat_val);
+  delay(20);
+
+  val_sum = 0;
+  
+  if(flat_val > trigger_value) {
     goIntoLoop = true;
   }
-
-  // reset distance values to 0
-  for (int i = 0; i < 5; i++ ) {
-    distance[i] = 0;
-  }
-
+  
   if(goIntoLoop) {
-    Cayenne.loop();
+    Cayenne.loop();  // comment out for debugging
   }
-
+  
 }
 
 CAYENNE_OUT(RETO_CHANNEL)
